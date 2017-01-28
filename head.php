13d0cb09fbab71c5b1cc95e123182af12e2dfab9
katilio <ammelie.neth@student.kyh.se>
2017-01-28 20:40:09 +0100

    fixed indentation

--- head.php
+++ head.php
@@ -8,7 +8,7 @@
       <link href='https://fonts.googleapis.com/css?family=Roboto:300|Share+Tech+Mono&amp;subset=latin-ext' rel='stylesheet'>
       <script src='https://code.jquery.com/jquery-3.1.1.min.js' integrity='sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=' crossorigin='anonymous'></script>
       <link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css'>
-<script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+      <script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
       <title>Ammelie Neth, Front End Development Student</title>
     </head>
   ";
