ca97a9d18d8f3930fd9258397c31fdaf367e3fc3
katilio <ammelie.neth@student.kyh.se>
2017-01-29 14:12:21 +0100

    did an oopsie

--- contact.php
+++ contact.php
@@ -1,5 +1,7 @@
 <?php
 
+  include 'config.php';
+
   echo "
     <div class='contact-wrapper'>
       <header class='contact-header bold-header'>
@@ -8,25 +10,8 @@
 
       <div class='contact-content'>
         <form action='' method='post'>
-          <label for='name'>Name</label>
-          <input type='text' name='name' required>
-          <br />
-          <label for='email'>Email</label>
-          <input type='email' name='email' required>
-          <br />
-          <label for='number'>Phone number</label>
-          <input type='tel' name='phone'>
-          <br />
-          <label for='message' class='align-top'>Message</label>
-          <textarea name='message' required></textarea>
-          <br />
-          <input type='submit' name='send-msg' class='btn' value='Send message'>
-        </form>
-      </div>
-    </div>
   ";
 
-  include 'config.php';
 
   if (isset($_POST['send-msg'])) {
     $date = date("Y-m-d H:i:s");
@@ -42,7 +27,30 @@
     ";
 
     mysqli_query($db, $query);
+
+    echo "<h2 class'thank-you'>Thank you for your message!</h2>";
   }
+
+
+  echo "
+        <label for='name'>Name</label>
+        <input type='text' name='name' required>
+        <br />
+        <label for='email'>Email</label>
+        <input type='email' name='email' required>
+        <br />
+        <label for='number'>Phone number</label>
+        <input type='tel' name='phone'>
+        <br />
+        <label for='message' class='align-top'>Message</label>
+        <textarea name='message' required></textarea>
+        <br />
+        <input type='submit' name='send-msg' class='btn' value='Send message'>
+      </form>
+    </div>
+  </div>
+";
+
 
 
   # Trims input and escapes it
