7493e681ab1345bf0206ae18a22b765c6c5fb5fd
katilio <ammelie.neth@student.kyh.se>
2017-01-29 20:08:02 +0100

    proper resizing of embedded video

--- about.php
+++ about.php
@@ -1,12 +1,12 @@
 <?php
 
-  echo '
-    <div class="wrapper about-wrapper">
-      <header class="about-header bold-header">
+  echo "
+    <div class='wrapper about-wrapper'>
+      <header class='about-header bold-header'>
         <h1>About me</h1>
       </header>
-      <div class="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/6WcRtA1795w" frameborder="0"></iframe></div>
-      <article class="content about-content">
+      <div class='video'><iframe src='https://www.youtube.com/embed/6WcRtA1795w' frameborder='0'></iframe></div>
+      <article class='content about-content'>
         <p>
           For a few years now, I&#39;ve been trying out various educations and jobs in the intersection between technology and language.
           I&#39;ve worked as a tester, a translator and a bit of both during my time in localization testing.
@@ -19,16 +19,16 @@
         <br />
         <p>
           If you or someone you know are looking for someone matching my
-          profile—especially if you&#39;d like an extra hand before summer in 2018—please don&#39;t hesitate to <a href="?page=contact.php">
+          profile—especially if you&#39;d like an extra hand before summer in 2018—please don&#39;t hesitate to <a href='?page=contact.php'>
           get in touch</a>! I&#39;d be happy to answer any questions you might have.
         </p>
       </article>
 
-      <article class="content trivia">
+      <article class='content trivia'>
         <h3>Connect with me or check what I&#39;m up to:</h3>
         <ul>
-          <li><a target="_blank" href="https://se.linkedin.com/in/ammelieneth">LinkedIn</a></li>
-          <li><a target="_blank" href="https://www.instagram.com/smorpopcorn/">Instagram</a></li>
+          <li><a target='_blank' href='https://se.linkedin.com/in/ammelieneth'>LinkedIn</a></li>
+          <li><a target='_blank' href='https://www.instagram.com/smorpopcorn/'>Instagram</a></li>
         </ul>
         <br />
 
@@ -42,6 +42,6 @@
         </ul>
       </article>
     </div>
-  ';
+  ";
 
 ?>
