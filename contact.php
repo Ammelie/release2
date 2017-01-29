ccf1be634bae4380290443eb7a3a39a3b48ce1c9
katilio <ammelie.neth@student.kyh.se>
2017-01-29 13:59:33 +0100

    message form now works

--- contact.php
+++ contact.php
@@ -1,29 +1,55 @@
 <?php
 
-  echo '
-    <div class="contact-wrapper">
-      <header class="contact-header bold-header">
+  echo "
+    <div class='contact-wrapper'>
+      <header class='contact-header bold-header'>
         <h1>Get in touch</h1>
       </header>
 
-      <div class="contact-content">
-        <form>
-          <label for="name">Name</label>
-          <input type="text" name="name" required></input>
+      <div class='contact-content'>
+        <form action='' method='post'>
+          <label for='name'>Name</label>
+          <input type='text' name='name' required>
           <br />
-          <label for="email">Email</label>
-          <input type="email" name="email" required></input>
+          <label for='email'>Email</label>
+          <input type='email' name='email' required>
           <br />
-          <label for="number">Phone number</label>
-          <input type="tel" name="number" required></input>
+          <label for='number'>Phone number</label>
+          <input type='tel' name='phone'>
           <br />
-          <label for="message" class="align-top">Message</label>
-          <textarea name="message" required></textarea>
+          <label for='message' class='align-top'>Message</label>
+          <textarea name='message' required></textarea>
           <br />
-          <input type="submit" name="send-msg" class="btn"></input>
+          <input type='submit' name='send-msg' class='btn' value='Send message'>
         </form>
       </div>
     </div>
-  ';
+  ";
+
+  include 'config.php';
+
+  if (isset($_POST['send-msg'])) {
+    $date = date("Y-m-d H:i:s");
+
+    # Run a cleaning function on all fields posted
+    foreach ($_POST as $key => $value) {
+      $$key = clean($db, $value);
+    }
+
+    $query = "
+    INSERT INTO messages (date, name, email, phone, message) VALUES
+    ('".$date."', '".$name."', '".$email."', '".$phone."', '".$message."')
+    ";
+
+    mysqli_query($db, $query);
+  }
+
+
+  # Trims input and escapes it
+  function clean($db, $value) {
+    $cleanVal = trim($value);
+    $cleanVal = mysqli_real_escape_string($db, $value);
+    return $cleanVal;
+  }
 
 ?>
