a15a469039a5965dd035494fbf84379998aeaeab
katilio <ammelie.neth@student.kyh.se>
2017-01-29 23:58:21 +0100

    portfolio in place

---
+++ portfolio.php
@@ -0,0 +1,34 @@
+<?php
+
+  echo "
+    <div class='portfolio-header-wrapper'>
+      <header class='portfolio-header bold-header'>
+        <h1>Portfolio</h1>
+      </header>
+    </div>
+
+    <div class='portfolio-content-wrapper'>
+      <div class='project-container'>
+        <a href='http://www.dengladageten.se'><img src='images/geten.jpg' alt='Den Glada Geten B&amp;B'></a>
+        <h3>Den Glada Geten B&amp;B</h3>
+        <p>First school project. Website for a small B&amp;B with functioning booking and admin system.</p>
+      </div>
+      <div class='project-container'>
+        <a href='#'><img src='images/sylvester.jpg' alt='A yawning cat'></a>
+        <h3>Oh no!</h3>
+        <p>More projects coming soon.</p>
+      </div>
+      <div class='project-container'>
+        <a href='#'><img src='images/sylvester.jpg' alt='A yawning cat'></a>
+        <h3>Oh no!</h3>
+        <p>More projects coming soon.</p>
+      </div>
+      <div class='project-container'>
+        <a href='#'><img src='images/sylvester.jpg' alt='A yawning cat'></a>
+        <h3>Oh no!</h3>
+        <p>More projects coming soon.</p>
+      </div>
+    </div>
+  ";
+
+?>
