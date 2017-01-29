6e02d2eb8275fa2c61ec55db0bee87396c9b058e
Ammelie <ammelie.neth@student.kyh.se>
2017-01-29 22:25:29 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  define('DB_SERVER', 'ammelieneth-219299.mysql.binero.se');
-  define('DB_USERNAME', '219299_ls66372');
-  define('DB_PASSWORD', 'lolDeadlines1');
-  define('DB_DATABASE', '219299-ammelieneth');
-  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-?>
